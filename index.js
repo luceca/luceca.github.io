25e49b1838277861e7307f7f3cbda6bdc7fd1c7d
luceca <crisalucero@gmail.com>
2019-08-22 15:56:31 -0700

    Want to change navbar after scrolling down 100vh

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 document.title = "Lucero | Portfolio";
 
+window.addEventListener("scroll", () => {
+    if(window.scrollY > window.innerHeight){
+        document.getElementById("navBar").style.background = 'red';
+    }
+})
