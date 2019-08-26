68e6523bcb072c3219826843a6fae718c010c3dc
CrisALucero <crisalucero@gmail.com>
2019-08-25 23:00:24 -0700

    Attempting to keep bg sticky until scroll change

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 document.title = "Lucero | Portfolio";
 
-window.addEventListener("scroll", () => {
-    if(window.scrollY > window.innerHeight){
-        document.getElementById("navBar").style.background = 'red';
-    }
-})
+
+/*TODO: Change to set fixed position of bg to sticky when top of headshot is reached */
+// window.onscroll = () =>{
+//     console.log(window.pageYOffset+window.innerHeight + 'vs' + window.innerHeight * 2);
+//     var h = document.getElementById('background');
+//     var about = document.getElementById('about');
+
+//     if(window.pageYOffset + window.innerHeight > window.innerHeight * 2){
+//         h.style.position = "absolute"
+//     } else {
+//         h.style.position = "fixed"
+//     }
+
+// }
